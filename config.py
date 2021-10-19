7ca934cb2d49693aef17435a777e3dfbb8803f1b
KAndHisC <takiw@graphcore.ai>
2021-10-19 05:17:08 +0000

    testing in official example

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import torch
 
 class CFG:
     debug = False
-    image_path = "/fsx/home/minghongc/datasets/Flickr8k/Flicker8k_Dataset"
-    captions_path = "/fsx/home/minghongc/datasets/Flickr8k"
+    # image_path = "/fsx/home/minghongc/datasets/Flickr8k/Flicker8k_Dataset"
+    image_path = "/fsx/home/minghongc/datasets/mm/images"
+    # captions_path = "/fsx/home/minghongc/datasets/Flickr8k"
+    captions_path = "/fsx/home/minghongc/datasets/mm/img_cap.txt"
     batch_size = 128
     num_workers = 8
 
@@ -17,7 +19,8 @@ class CFG:
 
     # scheduler
     epochs = 20
-    warmup_steps = 1000 # in paper
+    # warmup_steps = 2000 # in paper
+    warmup_steps = 65536
     # num_steps = epochs * (num of data / batch size)
     patience = 2 # ?
     factor = 0.5 # ?
--- tests/zero_shot_prediction.py
+++ tests/zero_shot_prediction.py
@@ -2,54 +2,32 @@ import os
 import clip
 import torch
 from torchvision.datasets import CIFAR100
-import numpy as np
 
 # Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load('ViT-B/32', device)
+# model, preprocess = clip.load('ViT-B/32', device)
+model, preprocess = clip.load("./models/2.2459481861016437.pt", device=device)
 
 # Download the dataset
-cifar100 = CIFAR100(root=os.path.expanduser("./data/cifar100"), download=True, train=True)
+cifar100 = CIFAR100(root=os.path.expanduser("~/.cache"), download=True, train=False)
 
+# Prepare the inputs
+image, class_id = cifar100[3637]
+image_input = preprocess(image).unsqueeze(0).to(device)
 text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in cifar100.classes]).to(device)
 
-
-r1_count = 0
-r5_count = 0
-a_value = []
 # Calculate features
 with torch.no_grad():
+    image_features = model.encode_image(image_input)
     text_features = model.encode_text(text_inputs)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
-    all_count = len(cifar100)
-    for i in range(all_count):
-        # i = 3637
-        # i = 1
-        # Prepare the inputs
-        image, class_id = cifar100[i]
-        image_input = preprocess(image).unsqueeze(0).to(device)
-        image_features = model.encode_image(image_input)
-        # Pick the top 5 most similar labels for the image
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-        
-        similarity = (100 * image_features @ text_features.t()).softmax(dim=-1)
-        value, recall_at_1 = similarity[0].topk(1)
-        # values, recall_at_5 = similarity.softmax(dim=-1)[0].topk(5)
-        # value = similarity.softmax(dim=-1)[0][class_id]
-        # print(value)
 
-        # print(indices,class_id, indices == class_id)
-        # exit()
-        if recall_at_1 == class_id:
-        # #     r1_count += 1
-            a_value.append(value.cpu().numpy())
-        # elif class_id in recall_at_5:
-        #     r5_count += 1
-        
-        if i % 1000 == 0:
-            print(i)
-        
-# print(r1_count/all_count, r5_count/all_count, r1_count, r5_count)
-print(np.array(a_value).mean())
+# Pick the top 5 most similar labels for the image
+image_features /= image_features.norm(dim=-1, keepdim=True)
+text_features /= text_features.norm(dim=-1, keepdim=True)
+similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+values, indices = similarity[0].topk(5)
 
-    
+# Print the result
+print("\nTop predictions:\n")
+for value, index in zip(values, indices):
+    print(f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%")
